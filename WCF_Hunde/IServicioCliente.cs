7aedb2273b3c3c1481ed49d4b0bfaa3c23f1090d
andresaleksandarmilutin@gmail.com <Franch98>
2021-05-27 22:38:57 -0500

    ServiceCliente

--- WCF_Hunde/IServicioCliente.cs
+++ WCF_Hunde/IServicioCliente.cs
@@ -12,6 +12,214 @@ namespace WCF_Hunde
     public interface IServicioCliente
     {
         [OperationContract]
-        void DoWork();
+        ClienteBE ConsultCliente(String strCod);
+
+        [OperationContract]
+        Boolean InsertCliente(ClienteBE objClienteBE);
+
+        [OperationContract]
+        Boolean UpdateCliente(ClienteBE objClienteBE);
+
+        [OperationContract]
+        Boolean DeleteCliente(String strCod);
+
+        [OperationContract]
+        List<ClienteBE> GetAllCliente();
     }
+
+    [DataContract]
+    [Serializable]
+
+    public class ClienteBE
+    {
+
+        //Datos privados de la tabla Tb_Cliente
+        private String mvarcod_cli;
+        private Int16 mvartipo_cliente;
+        //Adicion tipo especial para cliente
+        private String mvartipo_cli;
+
+        private String mvarnom_cliente;
+        private String mvarape_pat_cliente;
+        private String mvarape_mat_cliente;
+        //Adiccion campo especial para nombres
+        private String mvarapellidos_nombre;
+
+        private String mvardireccion_cliente;
+        private String mvarcel_cliente;
+        private String mvaremail_cliente;
+        private Int16 mvares_dueno;
+        //Adiccion campo especial para dueño
+        private String mvarduenopaciente;
+
+        private Int16 mvares_empleado;
+        //Adiccion campo especial para empleado
+        private String mvarcliente_empleado;
+
+        private String mvarid_ubigeo;
+        private String mvardni_cliente;
+        private String mvarusu_reg_cli;
+        private DateTime mvarfec_reg_cli;
+        private String mvarusu_ult_modificacion_cli;
+        private DateTime mvarfecha_ult_modificacion_cli;
+        private Int16 mvarestado_cli;
+        //Adiccion campo especial para empleado
+        private String mvarestadocliente;
+
+        [DataMember]
+        public String cod_cli
+        {
+            get { return mvarcod_cli; }
+            set { mvarcod_cli = value; }
+        }
+
+        [DataMember]
+        public Int16 Tipo_cliente
+        {
+            get { return mvartipo_cliente; }
+            set { mvartipo_cliente = value; }
+        }
+
+        [DataMember]
+        public String nom_cliente
+        {
+            get { return mvarnom_cliente; }
+            set { mvarnom_cliente = value; }
+        }
+
+        [DataMember]
+        public String ape_pat_cliente
+        {
+            get { return mvarape_pat_cliente; }
+            set { mvarape_pat_cliente = value; }
+        }
+
+        [DataMember]
+        public String ape_mat_cliente
+        {
+            get { return mvarape_mat_cliente; }
+            set { mvarape_mat_cliente = value; }
+        }
+
+        [DataMember]
+        public String direccion_cliente
+        {
+            get { return mvardireccion_cliente; }
+            set { mvardireccion_cliente = value; }
+        }
+
+        [DataMember]
+        public String cel_cliente
+        {
+            get { return mvarcel_cliente; }
+            set { mvarcel_cliente = value; }
+        }
+
+        [DataMember]
+        public String email_cliente
+        {
+            get { return mvaremail_cliente; }
+            set { mvaremail_cliente = value; }
+        }
+
+        [DataMember]
+        public Int16 es_dueno
+        {
+            get { return mvares_dueno; }
+            set { mvares_dueno = value; }
+        }
+
+        [DataMember]
+        public Int16 es_empleado
+        {
+            get { return mvares_empleado; }
+            set { mvares_empleado = value; }
+        }
+
+        [DataMember]
+        public String id_ubigeo
+        {
+            get { return mvarid_ubigeo; }
+            set { mvarid_ubigeo = value; }
+        }
+
+        [DataMember]
+        public String dni_cliente
+        {
+            get { return mvardni_cliente; }
+            set { mvardni_cliente = value; }
+        }
+
+        [DataMember]
+        public String usu_reg_cli
+        {
+            get { return mvarusu_reg_cli; }
+            set { mvarusu_reg_cli = value; }
+        }
+
+        [DataMember]
+        public DateTime fec_reg_cli
+        {
+            get { return mvarfec_reg_cli; }
+            set { mvarfec_reg_cli = value; }
+        }
+
+        [DataMember]
+        public String usu_ult_modificacion_cli
+        {
+            get { return mvarusu_ult_modificacion_cli; }
+            set { mvarusu_ult_modificacion_cli = value; }
+        }
+
+        [DataMember]
+        public DateTime fecha_ult_modificacion_cli
+        {
+            get { return mvarfecha_ult_modificacion_cli; }
+            set { mvarfecha_ult_modificacion_cli = value; }
+        }
+
+        [DataMember]
+        public Int16 estado_cli
+        {
+            get { return mvarestado_cli; }
+            set { mvarestado_cli = value; }
+        }
+
+        //Campos especiales
+        [DataMember]
+        public String tipo
+        {
+            get { return mvartipo_cli; }
+            set { mvartipo_cli = value; }
+        }
+
+        [DataMember]
+        public String dueno
+        {
+            get { return mvarduenopaciente; }
+            set { mvarduenopaciente = value; }
+        }
+
+        [DataMember]
+        public String empleado
+        {
+            get { return mvarcliente_empleado; }
+            set { mvarcliente_empleado = value; }
+        }
+
+        [DataMember]
+        public String estado
+        {
+            get { return mvarestadocliente; }
+            set { mvarestadocliente = value; }
+        }
+
+        [DataMember]
+        public String apellidos_nombre
+        {
+            get { return mvarapellidos_nombre; }
+            set { mvarapellidos_nombre = value; }
+        }
+    }
+
 }
--- WCF_Hunde/ServicioCliente.cs
+++ WCF_Hunde/ServicioCliente.cs
@@ -10,8 +10,222 @@ namespace WCF_Hunde
     // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioCliente" en el código y en el archivo de configuración a la vez.
     public class ServicioCliente : IServicioCliente
     {
-        public void DoWork()
+        public ClienteBE ConsultCliente(string strCod)
         {
+            HundeDBEntities MisClientes = new HundeDBEntities();
+            try
+            {
+                //Obtiene la istancia de cliente con LINQ
+                Tb_Cliente objConsulta = (from objCli in MisClientes.Tb_Cliente
+                                          where objCli.cod_cli == strCod
+                                          select objCli).FirstOrDefault();
+                //Crea una instancia del clienteBE para traer sus resultados
+                ClienteBE objCliente = new ClienteBE();
+
+                objCliente.cod_cli = objConsulta.cod_cli;
+
+                if (objConsulta.Tipo_cliente == 1)
+                {
+                    objCliente.tipo = "Persona Natural";
+                }
+                else
+                {
+                    objCliente.tipo = "Persona Juridica";
+                }
+                objCliente.nom_cliente = objConsulta.nom_cliente;
+                objCliente.ape_pat_cliente = objConsulta.ape_pat_cliente;
+                objCliente.ape_mat_cliente = objConsulta.ape_mat_cliente;
+                objCliente.direccion_cliente = objConsulta.direccion_cliente;
+                objCliente.email_cliente = objConsulta.email_cliente;
+                if (objConsulta.es_dueno == 1)
+                {
+                    objCliente.dueno = "Dueño";
+                }
+                else
+                {
+                    objCliente.dueno = "Familiar";
+                }
+
+                if (objConsulta.es_empleado == 1)
+                {
+                    objCliente.empleado = "Empleado";
+                }
+                else
+                {
+                    objCliente.empleado = "Cliente";
+                }
+
+                objCliente.dni_cliente = objConsulta.dni_cliente;
+
+                if (objConsulta.estado_cli == 1)
+                {
+                    objCliente.estado = "Activo";
+                }
+                else
+                {
+                    objCliente.estado = "Inactivo";
+                }
+
+                return objCliente;
+
+            }
+            catch (EntityException ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool DeleteCliente(string strCod)
+        {
+            HundeDBEntities MisClientes = new HundeDBEntities();
+            try
+            {
+                //Obtenemos con LINQ la instancia del cliente a actualizar
+                Tb_Cliente objCliente = (from objClient in MisClientes.Tb_Cliente
+                                         where objClient.cod_cli == strCod
+                                         select objClient).FirstOrDefault();
+
+                //Removemos el vendedor
+                MisClientes.Tb_Cliente.Remove(objCliente);
+                MisClientes.SaveChanges();
+
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+     
+        public List<ClienteBE> GetAllCliente()
+        {
+            HundeDBEntities MisClientes = new HundeDBEntities();
+            try
+            {
+                List<ClienteBE> objListaCliente = new List<ClienteBE>();
+                var query = (from objClient in MisClientes.Tb_Cliente
+                             select objClient);
+
+                foreach (var objCliente in query)
+                {
+                    //Creamos una instancia del cliente para retornar el resultado
+                    ClienteBE objClienteBE = new ClienteBE();
+
+                    objClienteBE.cod_cli = objCliente.cod_cli;
+                    objClienteBE.apellidos_nombre = objCliente.ape_pat_cliente + "," + objCliente.ape_mat_cliente + " , " + objCliente.nom_cliente;
+                    objClienteBE.direccion_cliente = objCliente.direccion_cliente;
+                    objClienteBE.cel_cliente = objCliente.cel_cliente;
+                    objClienteBE.email_cliente = objCliente.email_cliente;
+                    //objClienteBE.dueno = objCliente.es_dueno;
+                    if (objClienteBE.es_dueno == 1)
+                    {
+                        objClienteBE.dueno = "Dueño";
+                    }
+                    else
+                    {
+                        objClienteBE.dueno = "Familiar";
+                    }
+                    objClienteBE.dni_cliente = objCliente.dni_cliente;
+                    objClienteBE.estado_cli = Convert.ToInt16(objCliente.estado_cli);
+                    if (objClienteBE.estado_cli == 1)
+                    {
+                        objClienteBE.estado = "Activo";
+                    }
+                    else
+                    {
+                        objClienteBE.estado = "Inactivo";
+                    }
+
+                    objListaCliente.Add(objClienteBE);
+
+                }
+
+                return objListaCliente;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool InsertCliente(ClienteBE objClienteBE)
+        {
+            //Instanciamos el modelo
+            HundeDBEntities MisClientes = new HundeDBEntities();
+            try
+            {
+                //Crea la instancia del nuevo vendedor
+                Tb_Cliente objCliente = new Tb_Cliente();
+                //Asignamos las propiedades al nuevo cliente desde el parametro para que sea reconocido por el objeto
+                //Codigo vacio a ser llenado de forma automatica incremental
+                objCliente.cod_cli = String.Empty;
+                objCliente.Tipo_cliente = objClienteBE.Tipo_cliente;
+                objCliente.nom_cliente = objClienteBE.nom_cliente;
+                objCliente.ape_pat_cliente = objClienteBE.ape_pat_cliente;
+                objCliente.ape_mat_cliente = objClienteBE.ape_mat_cliente;
+                objCliente.direccion_cliente = objClienteBE.direccion_cliente;
+                objCliente.cel_cliente = objClienteBE.cel_cliente;
+                objCliente.email_cliente = objClienteBE.email_cliente;
+                objCliente.es_dueno = objClienteBE.es_dueno;
+                objCliente.es_empleado = objClienteBE.es_empleado;
+                objCliente.id_ubigeo = objClienteBE.id_ubigeo;
+                objCliente.dni_cliente = objClienteBE.dni_cliente;
+                objCliente.usu_reg_cli = objClienteBE.usu_reg_cli;
+                objCliente.fec_reg_cli = objClienteBE.fec_reg_cli;
+                objCliente.estado_cli = objClienteBE.estado_cli;
+
+                //Agrega nueva instancia
+                MisClientes.Tb_Cliente.Add(objCliente);
+                return true;
+
+            }
+            catch (EntityException ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool UpdateCliente(ClienteBE objClienteBE)
+        {
+            HundeDBEntities MisClientes = new HundeDBEntities();
+
+            try
+            {
+                //Obtenemos con LINQ el cliente para actualizar
+                Tb_Cliente objCliente = (from objClient in MisClientes.Tb_Cliente
+                                         where objClient.cod_cli == objClienteBE.cod_cli
+                                         select objClient).FirstOrDefault();
+
+                //el vendedor se encuentra identificado
+
+                //Asignamos las propiedades a la instancia de objCliente actualizar desde el parametro objClient
+                objCliente.cod_cli = objClienteBE.cod_cli;
+                objCliente.Tipo_cliente = objClienteBE.Tipo_cliente;
+                objCliente.nom_cliente = objClienteBE.nom_cliente;
+                objCliente.ape_pat_cliente = objClienteBE.ape_pat_cliente;
+                objCliente.ape_mat_cliente = objClienteBE.ape_mat_cliente;
+                objCliente.direccion_cliente = objClienteBE.direccion_cliente;
+                objCliente.cel_cliente = objClienteBE.cel_cliente;
+                objCliente.email_cliente = objClienteBE.email_cliente;
+                objCliente.es_dueno = objClienteBE.es_dueno;
+                objCliente.es_empleado = objClienteBE.es_empleado;
+                objCliente.id_ubigeo = objClienteBE.id_ubigeo;
+                objCliente.dni_cliente = objClienteBE.dni_cliente;
+                objCliente.estado_cli = objClienteBE.estado_cli;
+
+                //Agrega nueva instancia
+                MisClientes.SaveChanges();
+                return true;
+
+            }
+            catch (EntityException ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
         }
     }
 }
