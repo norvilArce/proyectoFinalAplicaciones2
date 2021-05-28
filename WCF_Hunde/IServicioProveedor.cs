a0271b4a486b9c7290c4a1759d46874d8a4c5060
Norvil Arce <norvil.arce@gmail.com>
2021-05-27 20:31:09 -0500

    agrego servicio proveedores

---
+++ WCF_Hunde/IServicioProveedor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Hunde
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioProveedor" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioProveedor
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
